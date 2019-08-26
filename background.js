2ad31d4341f05d3f853ef2b2a7b4bd5155ab1915
YinZengYong <yinzengyong@bytedance.com>
2019-08-27 01:16:55 +0800

    finish

--- background.js
+++ background.js
@@ -1,10 +1,5 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-87975985-1']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.from == 'content_script') {
@@ -21,7 +16,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         file: "jquery.min.js"
     }, function() {
         if (chrome.runtime.lastError) {
-            alert("Page Marker does not support new tab pages or the Chrome Web Store because they are reserved by Chrome. Please try again on a different website.");
+            alert("Wu's Ruler does not support new tab pages or the Chrome Web Store because they are reserved by Chrome. Please try again on a different website.");
         } else {
           chrome.tabs.executeScript(null, { file: "marker.js" }, function() {})
         }
